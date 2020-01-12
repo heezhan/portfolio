a72ee0e917d169e01be2d247f37af619c460cf19
heezhan <hhan1194@gmail.com>
2020-01-11 23:23:06 -0500

    homepage introductory message

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import '../styles/Homepage.css';
 
 class Homepage extends React.Component {
+    handleScroll = (event) => {
+        let element = event.target 
+        if (element.scrollHeight - element.scrollTop === element.clientHeight) {
+            console.log('hey')
+        } 
+    }
+
     render() {
         return (
-            <div>
-                Homepage
+            <div className="homepage" onScroll={this.handleScroll}>
+                <div className="intro">
+                    Hi, my name is Heejae. I am a full stack web developer in D.C.
+                </div>
             </div>
         )
     }
