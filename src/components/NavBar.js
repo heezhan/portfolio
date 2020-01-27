11bcdc664a289265196f96afbb76ca911a82d953
heezhan <hhan1194@gmail.com>
2020-01-27 18:06:38 -0500

    toggle menu working

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,22 +3,43 @@ import '../styles/NavBar.css'
 
 class NavBar extends React.Component {
     state = {
-        open: false 
+        active: ""
     }
 
-    toggleMenu = () => {
-        
+    toggleSidebar = () => {
+        console.log("hi")
+        if (this.state.active === "active") {
+            this.setState({
+                active: ""
+            })
+        } else {
+            this.setState({
+                active: "active"
+            })
+        }  
     }
 
     render() {
         return (
-            <div className="navbar">
-                <span className="home">Home</span>
-                <span className="about">About</span>
-                <span className="projects">Projects</span>
-                <span className="resume" onClick={toggleMenu}>
-                    <i class="fas fa-bars"></i>
-                </span>
+            // <div className="navbar">
+            //     <span className="home">Home</span>
+            //     <span className="about">About</span>
+            //     <span className="projects">Projects</span>
+            //     <span className="resume">Resume</span>
+            // </div>
+
+            <div id="sidebar" className={`${this.state.active}`}>
+                <div className="toggle-btn" onClick={this.toggleSidebar}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Resume </li>
+                </ul>
             </div>
         )
     }
