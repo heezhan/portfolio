1b631072e409bcd83fb32c44c88e1236014d29a0
heezhan <hhan1194@gmail.com>
2020-01-29 14:07:16 -0500

    updated about me

--- src/components/About.js
+++ src/components/About.js
@@ -11,21 +11,21 @@ class About extends React.Component {
             <div className={`about-page ${this.props.opacity}`}>
                 <div className="about">
                     <h2>
-                    <span className="emphasis">Heejae Han </span>is a full stack web developer with a background in Marketing and PR. As a Media and Comm undergrad, she was always fascinated by social media and the digital space. She took an introductory computer science class as an elective, which sparked her interest in programming. Rather than being a passive user of digital platforms, she really wanted to partake in the process of cultivating the digital landscape. She is proficient in Ruby on Rails, JavaScript and React/ Redux. She excited for what the synergy between her logical and creative side can produce.
+                        <span className="emphasis">Heejae Han</span> is a full stack web developer with a background in Marketing and PR. As a Media and Comm undergrad, she was always fascinated by social media and the digital space. She took an introductory computer science class as an elective, which sparked her interest in programming. Rather than being a passive user of digital platforms, she really wanted to partake in the process of cultivating the digital landscape. She is proficient in Ruby on Rails, JavaScript and React/ Redux. She excited for what the synergy between her logical and creative side can produce.
                     </h2>
 
                     <h3>
-                    Ruby on Rails, MVC Software Architecture, RESTful routes, SQL and PostgreSQL, test-writing frameworks such as RSpec and Capybara, Javascript, React, Redux, HTML, CSS, and Agile methodologies
+                        Ruby on Rails, JavaScript, React, Redux, MVC, RESTful Routes, APIs, SQL, PostgreSQL, RSpec, Capybara, HTML5, CSS, and Agile Methodologies
                     </h3>
-
-                    <div className="socials">
+                </div>
+                <div className="socials">
                     <a href="https://www.linkedin.com/in/heejae-han/">
                         <i class="fab fa-linkedin" />
                     </a>
+
                     <a href="https://github.com/heezhan">
                         <i class="fab fa-github-square" />
                     </a>
-                    </div>
                 </div>
             </div>
         )
