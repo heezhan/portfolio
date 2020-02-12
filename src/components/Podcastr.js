d3e0dd59ef60ae26276547d6c19bbb20f075060f
heezhan <hhan1194@gmail.com>
2020-02-12 15:15:32 -0500

    incorporated carousel

--- src/components/Podcastr.js
+++ src/components/Podcastr.js
@@ -3,12 +3,40 @@ import '../styles/ProjectDetails.css';
 
 class Podcastr extends React.Component {
     render() {
+        let data = [
+            {
+                des: "Homepage/ Log In page",
+                imgSrc: require('../images/workout_planner/login.png')
+            },
+            {
+                des: "Main page after log in",
+                imgSrc: require('../images/workout_planner/home.png')
+            },
+            {
+                des: "Exercise details page",
+                imgSrc: require('../images/workout_planner/exercise_detail.png')
+            },
+            {
+                des: "Routines page",
+                imgSrc: require('../images/workout_planner/routines.png')
+            },
+            {
+                des: "Routine details page",
+                imgSrc: require('../images/workout_planner/routine_detail.png')
+            }
+        ];
+        
         return (
-            <div className={`project-details ${this.props.opacity}`} onClick={this.props.resetToggle}>
-                <p className="project-title">
-                    Podcastr
-                </p>
-
+            <div className="container">
+                <div className="carousel">
+                    <CarouselSlider slideItems = {data} />
+                </div>
+                <div className="details">
+                    <h1 className="project-title">Podcastr</h1>
+                    <h2>React | Ruby on Rails | Semantic UI</h2>
+                    <h2>A web app with how-to workout videos to help users streamline their daily exercise routines</h2>
+                    <h2>Used React Router so that the single page web app could emulate a more dynamic, multi-page interface. Incorporated methods such as Promise.all to work around the asynchronous nature of fetch calls. Constructed with JS React, Ruby on Rails, PostgreSQL DB and designed with Semantic UI and custom CSS</h2>
+                </div>
             </div>
         )
     }
--- src/components/WorkoutPlanner.js
+++ src/components/WorkoutPlanner.js
@@ -41,12 +41,24 @@ class WorkoutPlanner extends React.Component {
     render() {
         let data = [
             {
-                des: "Homepage",
+                des: "Homepage/ Log In page",
                 imgSrc: require('../images/workout_planner/login.png')
             },
             {
-                des: "2",
+                des: "Main page after log in",
                 imgSrc: require('../images/workout_planner/home.png')
+            },
+            {
+                des: "Exercise details page",
+                imgSrc: require('../images/workout_planner/exercise_detail.png')
+            },
+            {
+                des: "Routines page",
+                imgSrc: require('../images/workout_planner/routines.png')
+            },
+            {
+                des: "Routine details page",
+                imgSrc: require('../images/workout_planner/routine_detail.png')
             }
         ];
         
@@ -56,12 +68,12 @@ class WorkoutPlanner extends React.Component {
                     <CarouselSlider slideItems = {data} />
                 </div>
                 <div className="details">
-                    <p>Workout Planner</p>
-                    <p>React, Ruby on Rails, Semantic UI</p>
-                    <p></p>
+                    <h1 className="project-title">Workout Planner</h1>
+                    <h2>React | Ruby on Rails | Semantic UI</h2>
+                    <h2>A web app with how-to workout videos to help users streamline their daily exercise routines</h2>
+                    <h2>Used React Router so that the single page web app could emulate a more dynamic, multi-page interface. Incorporated methods such as Promise.all to work around the asynchronous nature of fetch calls. Constructed with JS React, Ruby on Rails, PostgreSQL DB and designed with Semantic UI and custom CSS</h2>
                 </div>
             </div>
-            
         )
         // return (
         //     <div className={`project-details ${this.props.opacity}`} onClick={this.props.resetToggle}>
